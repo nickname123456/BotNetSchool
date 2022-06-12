5f505a30cbea7bbd57bab8e3dc6c684272acb159
nickname123456 <71244456+nickname123456@users.noreply.github.com>
2022-06-12 17:49:55 +0300

    рефакторинг объявлений

--- commands/announcements/announcements.py
+++ commands/announcements/announcements.py
@@ -29,66 +29,51 @@ async def private_announcements(message: Message, amount=3):
             db.get_account_password(user_id),
             db.get_account_school(user_id),
             studentId)
-        logging.info(f'{message.peer_id}: Login in NetSchool')
-        # Копируем объявления из сго
-        announcements = await api.announcements()
-
-        # Берем только те объявления, которые нужны
-        announcements = announcements[:int(amount)]
-
-        # Если есть объявления:
-        if announcements:
-            # Приводим объявления в нужный вид
-            announcement = ''
-            for i in announcements:
-                announcement = "Дата: " + i['postDate'] +"\n"+ i['name'] + ":" + i['description']
-
-                announcement = re.sub(r'\<[^>]*\>', '', announcement)
-
-                # Отправляем объявление
-                await message.answer(announcement)
-                logging.info(f'{message.peer_id}: Send announcement')
-
-                # Перебераем прикрепленные файлы
-                for attachment in i['attachments']:
-                    # Скачиваем файл
-                    file = await api.download_attachment_as_bytes(attachment)
-
-                    # Отправляем файл
-                    attach = await DocMessagesUploader(api=message.ctx_api).upload(file_source = file,title = attachment['name'] ,peer_id=message.peer_id)
-                    await message.answer(attachment=attach)
-                    logging.info(f'{message.peer_id}: Send attachment')
-
-
-                # Делаем пробел между объявлениями
-                await message.answer('&#12288;')
-
-        # Если нет объявлений:
-        else:
-            logging.info(f'{message.peer_id}: No announcements')
-            await message.answer('❌Нет объявлений!')
-
-
-        await api.logout()
-        logging.info(f'{message.peer_id}: Logout from NetSchool')
     except: # если произошла ошибка
         logging.exception(f'{message.peer_id}: Exception occurred')
         await message.answer('Ты не зарегистрирован! \nНапиши "Начать"\n Или у тебя неверный логин/пароль')
         await api.logout()
         return
+    logging.info(f'{message.peer_id}: Login in NetSchool')
 
+    # Копируем объявления из сго
+    announcements = await api.announcements()
+    # Берем только те объявления, которые нужны
+    announcements = announcements[:int(amount)]
 
+    # Если есть объявления:
+    if announcements:
+        # Приводим объявления в нужный вид
+        announcement = ''
+        for i in announcements:
+            announcement = "Дата: " + i['postDate'] +"\n"+ i['name'] + ":" + i['description']
 
+            announcement = re.sub(r'\<[^>]*\>', '', announcement)
 
+            # Отправляем объявление
+            await message.answer(announcement)
+            logging.info(f'{message.peer_id}: Send announcement')
 
+            # Перебераем прикрепленные файлы
+            for attachment in i['attachments']:
+                # Скачиваем файл
+                file = await api.download_attachment_as_bytes(attachment)
 
+                # Отправляем файл
+                attach = await DocMessagesUploader(api=message.ctx_api).upload(file_source = file,title = attachment['name'] ,peer_id=message.peer_id)
+                await message.answer(attachment=attach)
+                logging.info(f'{message.peer_id}: Send attachment')
 
+            # Делаем пробел между объявлениями
+            await message.answer('&#12288;')
 
+    # Если нет объявлений:
+    else:
+        logging.info(f'{message.peer_id}: No announcements')
+        await message.answer('❌Нет объявлений!')
 
-
-
-
-
+    await api.logout()
+    logging.info(f'{message.peer_id}: Logout from NetSchool')
 
 
 
@@ -98,8 +83,8 @@ async def chat_announcements(message: Message, amount=3):
     logging.info(f'{message.peer_id}: I get "announcements {amount}"')
     # Айди чата:
     chat_id = message.chat_id
-
     studentId = db.get_chat_studentId(chat_id)
+
     try:
         # Логинимся в сго
         api = NetSchoolAPI(db.get_chat_link(chat_id))
@@ -108,50 +93,48 @@ async def chat_announcements(message: Message, amount=3):
             db.get_chat_password(chat_id),
             db.get_chat_school(chat_id),
             studentId)
-        logging.info(f'{message.peer_id}: Login in NetSchool')
-        # Копируем объявления из сго
-        announcements = await api.announcements()
-
-        # Берем только те объявления, которые нужны
-        announcements = announcements[:int(amount)]
-
-        # Если есть объявления:
-        if announcements:
-            # Приводим объявления в нужный вид
-            announcement = ''
-            for i in announcements:
-                announcement = "Дата: " + i['postDate'] +"\n"+ i['name'] + ":" + i['description']
-
-                announcement = re.sub(r'\<[^>]*\>', '', announcement)
-
-                # Отправляем объявление
-                await message.answer(announcement)
-                logging.info(f'{message.peer_id}: Send announcement')
-
-                # Перебераем прикрепленные файлы
-                for attachment in i['attachments']:
-                    # Скачиваем файл
-                    file = await api.download_attachment_as_bytes(attachment)
-
-                    # Отправляем файл
-                    attach = await DocMessagesUploader(api=message.ctx_api).upload(file_source = file,title = attachment['name'] ,peer_id=message.peer_id)
-                    await message.answer(attachment=attach)
-                    logging.info(f'{message.peer_id}: Send attachment')
-
-
-                # Делаем пробел между объявлениями
-                await message.answer('&#12288;')
-
-        # Если нет объявлений:
-        else:
-            logging.info(f'{message.peer_id}: No announcements')
-            await message.answer('❌Нет объявлений!')
-
-
-        await api.logout()
-        logging.info(f'{message.peer_id}: Logout from NetSchool')
     except: # если произошла ошибка
         logging.exception(f'{message.peer_id}: Exception occurred')
         await message.answer('Ты не зарегистрирован! \nНапиши "Начать"\n Или у тебя неверный логин/пароль')
         await api.logout()
         return
+    logging.info(f'{message.peer_id}: Login in NetSchool')
+
+    # Копируем объявления из сго
+    announcements = await api.announcements()
+    # Берем только те объявления, которые нужны
+    announcements = announcements[:int(amount)]
+
+    # Если есть объявления:
+    if announcements:
+        # Приводим объявления в нужный вид
+        announcement = ''
+        for i in announcements:
+            announcement = "Дата: " + i['postDate'] +"\n"+ i['name'] + ":" + i['description']
+
+            announcement = re.sub(r'\<[^>]*\>', '', announcement)
+
+            # Отправляем объявление
+            await message.answer(announcement)
+            logging.info(f'{message.peer_id}: Send announcement')
+
+            # Перебераем прикрепленные файлы
+            for attachment in i['attachments']:
+                # Скачиваем файл
+                file = await api.download_attachment_as_bytes(attachment)
+
+                # Отправляем файл
+                attach = await DocMessagesUploader(api=message.ctx_api).upload(file_source = file,title = attachment['name'] ,peer_id=message.peer_id)
+                await message.answer(attachment=attach)
+                logging.info(f'{message.peer_id}: Send attachment')
+
+            # Делаем пробел между объявлениями
+            await message.answer('&#12288;')
+
+    # Если нет объявлений:
+    else:
+        logging.info(f'{message.peer_id}: No announcements')
+        await message.answer('❌Нет объявлений!')
+
+    await api.logout()
+    logging.info(f'{message.peer_id}: Logout from NetSchool')
