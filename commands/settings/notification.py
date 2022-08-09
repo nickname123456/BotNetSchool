a3b0a34d3f8bf80afe331d767258dc7a500532b1
nickname123456 <71244456+nickname123456@users.noreply.github.com>
2022-08-09 12:14:48 +0500

    Рефакторинг уведомлений

--- commands/settings/notification.py
+++ commands/settings/notification.py
@@ -9,101 +9,88 @@ from settings import admin_id
 async def notification(bot):
     logging.info(f'Started mailing')
 
-    try:
-        users = db.get_accounts_mark_notification()
-        for user in users:
-            #try:
-            if user[8]: # Проверка на правильный логин и пароль
-                user_id = user[0]
-                marks, result = await getMarkNotify(
-                    db.get_account_login(user_id),
-                    db.get_account_password(user_id),
-                    db.get_account_school(user_id),
-                    db.get_account_link(user_id),
-                    db.get_account_old_mark(user_id)
-                )
-
-                db.edit_account_old_mark(user_id, marks)
-                db.commit()
-                for mark in result[-15:]:
-                    await bot.api.messages.send(message=mark, user_id=user_id, random_id=0)
-                    await asyncio.sleep(1)	
-            #except Exception as e:
-            #    pass
-        
-
-        users = db.get_accounts_announcements_notification()
-        for user in users:
-            #try:
-            if user[8]: # Проверка на правильный логин и пароль
-                user_id = user[0]
-                announcements, result = await getAnnouncementsNotify(
-                    db.get_account_login(user_id),
-                    db.get_account_password(user_id),
-                    db.get_account_school(user_id),
-                    db.get_account_link(user_id),
-                    db.get_account_studentId(user_id),
-                    db.get_account_old_announcements(user_id)
-                )
-
-                db.edit_account_old_announcements(user_id, announcements)
-                db.commit()
-                for announcement in result:
-                    await bot.api.messages.send(message=announcement, user_id=user_id, random_id=0)			
-                    await asyncio.sleep(1)
-            #except Exception as e:
-            #    pass
-
-
-
-
-
-        chats = db.get_chats_mark_notification()
-        for chat in chats:
-            #try:
-            chat_id = chat[0]
+    #try:
+    users = db.get_accounts_mark_notification()
+    for user in users:
+        #try:
+        if user[8]: # Проверка на правильный логин и пароль
+            user_id = user[0]
             marks, result = await getMarkNotify(
-                db.get_chat_login(chat_id),
-                db.get_chat_password(chat_id),
-                db.get_chat_school(chat_id),
-                db.get_chat_link(chat_id),
-                db.get_chat_old_mark(chat_id)
+                db.get_account_login(user_id),
+                db.get_account_password(user_id),
+                db.get_account_school(user_id),
+                db.get_account_link(user_id),
+                db.get_account_old_mark(user_id)
             )
 
-            db.edit_chat_old_mark(chat_id, marks)
+            db.edit_account_old_mark(user_id, marks)
             db.commit()
             for mark in result[-15:]:
-                await bot.api.messages.send(message=mark, peer_id=2000000000+chat_id, random_id=0)
+                await bot.api.messages.send(message=mark, user_id=user_id, random_id=0)
                 await asyncio.sleep(1)
-            #except Exception as e:
-            #    pass
-        
-        
-        chats = db.get_chats_announcements_notification()
-        for chat in chats:
-            #try:
-            chat_id = chat[0]
+    
+
+    users = db.get_accounts_announcements_notification()
+    for user in users:
+        #try:
+        if user[8]: # Проверка на правильный логин и пароль
+            user_id = user[0]
             announcements, result = await getAnnouncementsNotify(
-                db.get_chat_login(chat_id),
-                db.get_chat_password(chat_id),
-                db.get_chat_school(chat_id),
-                db.get_chat_link(chat_id),
-                db.get_chat_studentId(chat_id),
-                db.get_chat_old_announcements(chat_id)
+                db.get_account_login(user_id),
+                db.get_account_password(user_id),
+                db.get_account_school(user_id),
+                db.get_account_link(user_id),
+                db.get_account_studentId(user_id),
+                db.get_account_old_announcements(user_id)
             )
 
-            db.edit_chat_old_announcements(chat_id, announcements)
+            db.edit_account_old_announcements(user_id, announcements)
             db.commit()
             for announcement in result:
-                await bot.api.messages.send(message=announcement, peer_id=2000000000+chat_id, random_id=0)			
+                await bot.api.messages.send(message=announcement, user_id=user_id, random_id=0)			
                 await asyncio.sleep(1)
-            #except Exception as e:
-            #    pass
 
 
+
+
+
+    chats = db.get_chats_mark_notification()
+    for chat in chats:
+        #try:
+        chat_id = chat[0]
+        marks, result = await getMarkNotify(
+            db.get_chat_login(chat_id),
+            db.get_chat_password(chat_id),
+            db.get_chat_school(chat_id),
+            db.get_chat_link(chat_id),
+            db.get_chat_old_mark(chat_id)
+        )
+
+        db.edit_chat_old_mark(chat_id, marks)
+        db.commit()
+        for mark in result[-15:]:
+            await bot.api.messages.send(message=mark, peer_id=2000000000+chat_id, random_id=0)
+            await asyncio.sleep(1)        
+    
+    chats = db.get_chats_announcements_notification()
+    for chat in chats:
+        chat_id = chat[0]
+        announcements, result = await getAnnouncementsNotify(
+            db.get_chat_login(chat_id),
+            db.get_chat_password(chat_id),
+            db.get_chat_school(chat_id),
+            db.get_chat_link(chat_id),
+            db.get_chat_studentId(chat_id),
+            db.get_chat_old_announcements(chat_id)
+        )
+
+        db.edit_chat_old_announcements(chat_id, announcements)
+        db.commit()
+        for announcement in result:
+            await bot.api.messages.send(message=announcement, peer_id=2000000000+chat_id, random_id=0)			
+            await asyncio.sleep(1)
 
         logging.info(f'I sleep for 60 minutes')
-        #await asyncio.sleep(600)
 
-    except:
-        await bot.api.messages.send(message='У нас тут это... Ошибка в РАССЫЛКЕ!!! \nЧЕКАЙ ХЕРОКУ ЛОГИ', user_id=admin_id, random_id=0)
+    #except:
+    #    await bot.api.messages.send(message='У нас тут это... Ошибка в РАССЫЛКЕ!!! \nЧЕКАЙ ХЕРОКУ ЛОГИ', user_id=admin_id, random_id=0)
