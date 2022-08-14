97a7576a4b446e61a1e49d6bb6dd3f4ecc16c826
nickname123456 <71244456+nickname123456@users.noreply.github.com>
2022-08-14 19:35:07 +0500

    Рефакторинг исправления оценок

--- commands/marks/correction_mark_choice_mark.py
+++ commands/marks/correction_mark_choice_mark.py
@@ -3,19 +3,21 @@ from vkbottle.bot import Message, Blueprint
 from PostgreSQLighter import db
 from vkbottle import Keyboard, KeyboardButtonColor, Text
 import logging
+from VKRules import PayloadStarts
 
 
 bp = Blueprint('correction_mark_choice_mark') # Объявляем команду
 bp.on.vbml_ignore_case = True # Игнорируем регистр сообщений
+bp.labeler.custom_rules["PayloadStarts"] = PayloadStarts
 
 
 
-@bp.on.private_message(payload={'cmd': 'correction_mark_choice_mark'})
+@bp.on.private_message(PayloadStarts='{"cmd":"correction_choice_mark_')
 async def private_correction_mark_choice_mark(message: Message):
     logging.info(f'{message.peer_id}: I get correction_mark_choice_mark')
     user_id = message.from_id # ID юзера
 
-    lesson = message.text # Получаем нужный урок
+    lesson = message.payload[31:-2] # Получаем нужный урок
 
     db.edit_account_correction_lesson(user_id, lesson) # Изменяем урок в бд
     db.commit()
@@ -44,13 +46,13 @@ async def private_correction_mark_choice_mark(message: Message):
 
 
 
-@bp.on.chat_message(payload={'cmd': 'correction_mark_choice_mark'})
+@bp.on.chat_message(PayloadStarts='{"cmd":"correction_choice_mark_')
 async def chat_correction_mark_choice_mark(message: Message):
     logging.info(f'{message.peer_id}: I get correction_mark_choice_mark')
     # Айди чата:
     chat_id = message.chat_id
     
-    lesson = message.text # Получаем нужный урок
+    lesson = message.payload[31:-2] # Получаем нужный урок
 
     db.edit_chat_correction_lesson(chat_id, lesson) # Изменяем урок
     db.commit()
