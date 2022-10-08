4e2c76ce0b7a99d4d16c450320309507eda905f1
nickname123456 <71244456+nickname123456@users.noreply.github.com>
2022-10-08 16:28:04 +0500

    ADD: logging in tg bot

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -6,6 +6,7 @@ from tg_bot.filters import register_all_filters
 from tg_bot.handlers import register_all_handlers
 
 from settings import tg_token
+import logging
 
 
 async def __on_start_up(dp: Dispatcher) -> None:
@@ -14,6 +15,17 @@ async def __on_start_up(dp: Dispatcher) -> None:
 
 
 def start_tg_bot():
+    logging.basicConfig(level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+    print('')
+    print('-------------------------------')
+    print('  Скрипт Сетевого Города в ВК запущен.')
+    print('  Разработчик: Кирилл Арзамасцев ')
+    print('  https://vk.com/kirillarz')
+    print('-------------------------------')
+    print('')
+
     bot = Bot(token=tg_token, parse_mode='HTML')
     dp = Dispatcher(bot, storage=MemoryStorage())
     executor.start_polling(dp, skip_updates=True, on_startup=__on_start_up)
